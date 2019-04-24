4858b8950d33281291b36c990295327129d084d4
Bukilan <golod.egor@mail.ru>
2019-04-24 10:34:13 +0300

    Updates

---
+++ precache-manifest.4a5f3f831af40e24a127e51d05b32d1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fefa675cc354ca782fd005e859acdb",
+    "url": "/TestBuildMovieList/index.html"
+  },
+  {
+    "revision": "380311da596d7d22dc56",
+    "url": "/TestBuildMovieList/static/css/main.010eb74a.chunk.css"
+  },
+  {
+    "revision": "3ab44b7ab14b75ecb666",
+    "url": "/TestBuildMovieList/static/js/2.4908e65f.chunk.js"
+  },
+  {
+    "revision": "380311da596d7d22dc56",
+    "url": "/TestBuildMovieList/static/js/main.39e26614.chunk.js"
+  },
+  {
+    "revision": "8998593eea0d3dbd1c09",
+    "url": "/TestBuildMovieList/static/js/runtime~main.717d2f6c.js"
+  }
+]);
